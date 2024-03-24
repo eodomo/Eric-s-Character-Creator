9727ac8089e0b09b995e42a72fe1cee96dd557ae
Eric Olerud <eric@olerud.com>
2024-03-24 17:22:37 -0400

    create autoload enums

---
+++ Scripts/_enums.gd
@@ -0,0 +1,31 @@
+extends Node
+
+enum AbilityScore {
+    STR,
+    DEX,
+    CON,
+    INT,
+    WIS,
+    CHA
+}
+
+enum Skills {
+    ACROBATICS,
+    ANIMAL_HANDLING,
+    ARCANA,
+    ATHLETICS,
+    DECEPTION,
+    HISTORY,
+    INSIGHT,
+    INTIMIDATION,
+    INVESTIGATION,
+    MEDICINE,
+    NATURE,
+    PERCEPTION,
+    PERFORMANCE,
+    PERSUASION,
+    RELIGION,
+    SLEIGHT_OF_HAND,
+    STEALTH,
+    SURVIVAL
+}
