d075d47c76c6ed6f8839e82b025defb85efb101f
Eric Olerud <eric@olerud.com>
2024-03-24 12:51:36 -0400

    fix type error

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -11,11 +11,9 @@ var race : Race :
     set(value):
         race = value
         emit_signal("race_changed")
-var character_classes : Array[CharacterClass]
-var race : Race
 
 func get_character_level () -> int:
     var level : int = 0
-    for character_class in character_classes:
+    for character_class: CharacterClass in character_classes:
         level += character_class.level
     return level
