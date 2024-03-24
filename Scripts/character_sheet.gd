b959bbcaf1a532115d452771405bd5639103eb01
Eric Olerud <eric@olerud.com>
2024-03-24 11:02:55 -0400

    create item class

--- Scripts/character_sheet.gd
+++ Scripts/character_sheet.gd
@@ -6,13 +6,13 @@ class_name CharacterSheet extends Node2D
 @onready var race_label : Label = %RaceLabel
 
 func set_character_name(character_name : String) -> void:
-    character_name_label.text = character_name
-    
+	character_name_label.text = character_name
+	
 func set_character_level(character_level : int) -> void:
-    character_level_label.text = "Character Level: " + str(character_level)
-    
+	character_level_label.text = "Character Level: " + str(character_level)
+	
 func set_experience(experience : int) -> void:
-    experience_label.text = "Experience: " + str(experience)
+	experience_label.text = "Experience: " + str(experience)
 
 func set_race(race : Race) -> void:
-    race_label.text = race.name
+	race_label.text = race.name
