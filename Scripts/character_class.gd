f57dca9f0a3c65e3b5977f00421e7a7023439cba
Eric Olerud <eric@olerud.com>
2024-03-27 22:29:41 -0400

    put function in wrong file

--- Scripts/character_class.gd
+++ Scripts/character_class.gd
@@ -2,3 +2,9 @@ class_name CharacterClass extends Resource
 
 var level : int
 var experience_points : int
+
+## TODO: Write this function
+func get_character_data() -> Dictionary :
+	var data: Dictionary = {}
+
+	return data
