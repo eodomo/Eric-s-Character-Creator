baa08d1ebf36cdb89dc8cc00423ac5985f39ccf4
Eric Olerud <eric@olerud.com>
2024-03-27 22:44:49 -0400

    create spell resource

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -3,6 +3,8 @@ class_name Character extends Resource
 var name: String
 var character_classes: Array = [CharacterClass]
 var race : Race 
-var stats : Dictionary = {
-    ENUMS.
-}
+var base_stats : Dictionary
+var skill_proficiencies: Dictionary
+var known_spells: Array = [Spell]
+var max_spell_slots: int
+var available_spell_slots : int
---
+++ Scripts/spell.gd
@@ -0,0 +1,5 @@
+class_name Spell extends Resource
+
+var name: String
+var description: String
+var level: int
